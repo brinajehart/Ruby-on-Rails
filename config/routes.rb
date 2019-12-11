5c259dc8660d0fe86febed0467207403bd25637e
BrinaJEHART <54882871+BrinaJEHART@users.noreply.github.com>
2019-12-11 09:14:11 +0100

    Friends controller and login design

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   root to: 'posts#index'
 
   get '/profile', to: 'profile#index'
+  get '/friends', to: 'friends#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
