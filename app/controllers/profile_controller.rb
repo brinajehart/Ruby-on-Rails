e69c3f0c8f854983dbc9fcf50689041a50afde85
BrinaJEHART <54882871+BrinaJEHART@users.noreply.github.com>
2020-03-23 23:40:46 +0100

    dropdown menu fixes

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -3,4 +3,10 @@ class ProfileController < ApplicationController
         @USER=current_user 
         @posts= Post.where(user: current_user)
     end
+    
+    def other
+        @USER=User.find(params[:id])
+        @posts= Post.where(user: User.find(params[:id]))
+        render :template => "profile/index"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   get '/profile', to: 'profile#index'
   get '/friends', to: 'friends#index'
+  get '/profile/:id', to: 'profile#other'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   mount Commontator::Engine => '/commontator'
 end
